364f34db6c03b33e36650c2543ebbb28ed28f96b
samadovoybek758 <oybek22samadov@gmail.com>
2024-06-02 07:44:53 +0500

    end

---
+++ js/index.js
@@ -0,0 +1,148 @@
+// 1-masala
+// let num = + prompt("sonni kiriting(3 xonali)");
+// let y =Math.trunc(num/100);
+// console.log(y)
+
+
+
+// 2-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+
+// console.log(bir);
+// console.log(on);
+
+
+
+// 3-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+// let y =Math.trunc(num/100);
+
+// console.log(bir+on+y);
+
+
+
+// 4-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+// let y =Math.trunc(num/100);
+
+// console.log(bir*100+on*10+y);
+
+
+
+// 5-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+// let y =Math.trunc(num/100);
+
+// console.log(on*100+bir*10+y);
+
+
+
+// 6-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+// let y =Math.trunc(num/100);
+
+// console.log(bir*100+y*10+on);
+
+
+
+// 7-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+// let y =Math.trunc(num/100);
+
+// console.log(on*100+y*10+bir);
+
+
+
+// 8-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let bir =num%10;
+// let a =num%100;
+// let on =(a-bir)/10;
+// let y =Math.trunc(num/100);
+
+// console.log(y*100+bir*10+on);
+
+
+
+// 9-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let a =num%1000;
+// let y =Math.trunc(a/100);
+
+// console.log(y);
+
+
+
+// 10-masala
+// let num = + prompt("sonni kiritng(3 xonali)")
+// let a =num%10000;
+// let m =Math.trunc(a/1000);
+
+// console.log(m);
+
+
+
+// 11-masala
+// let s = + prompt("sekundni kiritng");
+// let m = Math.trunc(s/60);
+
+// console.log(m);
+
+
+
+// 12-masala
+// let s = + prompt("sekundni kiritng");
+// let soat = Math.trunc(s/3600);
+
+// console.log(soat);
+
+
+
+// 13-masala
+// let s = + prompt("sekundni kiritng");
+// let m = Math.trunc(s/60);
+// let a = s-(m*60);
+
+// console.log(m+ "minut");
+// console.log(a+ "sekund");
+
+
+
+// 14-masala
+// let s = + prompt("sekundni kiritng");
+// let soat = Math.trunc(s/3600);
+// let a = s-(soat*3600);
+
+// console.log(soat+ "soat");
+// console.log(a+ "sekund");
+
+
+
+// 15-masala
+// let s = + prompt("sekundni kiritng");
+// let soat = Math.trunc(s/3600);
+// let b = s-(soat*3600)
+// let m = Math.trunc(b/60);
+// let a = b-(m*60);
+
+// console.log(soat+ "soat");
+// console.log(m+ "minut");
+// console.log(a+ "sekund");
